5e29db5a893eac7ed20104865b9ab7841fe2b633
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-07-17 10:52:02 +0700

    ubah

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
-document.getElementById('satu').innerHTML='Bismillah';
+function hidupkan() {
+    document.getElementById('dor').src='lampunyala.jpg'
+}
+function matikan () {
+    document.getElementById('dor').src='lampumati.jpg';
+    document.getElementById('dir').style.backgroundColor="black";
+    document.getElementById('dir').style.opacity=1;
+}
+function slambu () {
+    document.getElementById('dir').style.opacity=0.3;
+    document.getElementById('dir').style.backgroundColor="yellow";
+}
