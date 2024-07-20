810bcaab9a67d40672c7fa4594ff8d193e389f3a
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-07-20 21:56:18 +0700

    ubah

---
+++ code.js
@@ -0,0 +1,11 @@
+function nyala() {
+    document.getElementById('faiz').src="lampunyala.jpg";
+    document.getElementById('slambu').style.opacity=0.2;
+    document.getElementById('slambu').style.backgroundColor="yellow";
+}
+function mati() {
+    document.getElementById('faiz').src="lampumati.jpg";
+    document.getElementById('slambu').style.opacity=1;
+    document.getElementById('slambu').style.backgroundColor="black";
+    
+}
