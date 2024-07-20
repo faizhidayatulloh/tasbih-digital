32978e88660347bba055b803614dc9161100322d
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-07-20 22:55:07 +0700

    ubah

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function nyala() {
     document.getElementById('faiz').src="lampunyala.jpg";
     document.getElementById('slambu').style.opacity=0.2;
@@ -8,18 +7,4 @@ function mati() {
     document.getElementById('faiz').src="lampumati.jpg";
     document.getElementById('slambu').style.opacity=1;
     document.getElementById('slambu').style.backgroundColor="black";
-    
-=======
-function hidupkan() {
-    document.getElementById('dor').src='lampunyala.jpg'
-}
-function matikan () {
-    document.getElementById('dor').src='lampumati.jpg';
-    document.getElementById('dir').style.backgroundColor="black";
-    document.getElementById('dir').style.opacity=1;
-}
-function slambu () {
-    document.getElementById('dir').style.opacity=0.3;
-    document.getElementById('dir').style.backgroundColor="yellow";
->>>>>>> ba77b33c9749b6be76a913849fdd2e1d9b9bdd61
 }
